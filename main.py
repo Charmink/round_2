aa97b7ce8849603ec6a65fb66d39fb06a4dad614
Charmink <mry2002@mail.ru>
2019-11-21 23:20:37 +0300

    final_commit

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QColor
-from random import choice
-from PyQt5 import uic
+import pygame
+
+# инициализация Pygame:
+pygame.init()
+# размеры окна:
+size = width, height = map(int, input().split())
+# screen — холст, на котором нужно рисовать:
+screen = pygame.display.set_mode(size)
+# формирование кадра:
+# команды рисования на холсте
 
 
-class Example(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(300, 300, 400, 300)
-        uic.loadUi('UI.ui', self)
-        self.setWindowTitle('Рисование')
-        self.show()
-        self.figure = False
-        self.pushButton.clicked.connect(self.paint)
-
-    def paint(self):
-        self.figure = True
-        self.update()
-
-    def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.drawKrug(qp)
-        qp.end()
-
-    def drawKrug(self, qp):
-        if self.figure:
-            qp.setBrush(QColor(255, 255, 0))
-            d = choice(range(10, 50))
-            qp.drawEllipse(choice(range(400)), choice(range(400)), d, d)
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+def draw():
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 50)
+    text = font.render("Hello, Pygame!", 1, (100, 255, 100))
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+                                           text_w + 20, text_h + 20), 1)
+# смена (отрисовка) кадра:
+pygame.display.flip()
+# ожидание закрытия окна:
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+# завершение работы:
+pygame.quit()
